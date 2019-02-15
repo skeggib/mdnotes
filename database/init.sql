f0fbd4bfc2848162e3b3f9122ffc5d18dcb2e164
Sebastien Klasa <klasa.sebastien@gmail.com>
2019-02-15 08:39:29 +0000

    Add database init script and database Dockerfile

---
+++ database/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users
+(
+    name VARCHAR(256),
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP,
+
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE notes
+(
+    id SERIAL,
+    title VARCHAR(4096) NOT NULL,
+    content TEXT NOT NULL,
+    owner VARCHAR(256) NOT NULL,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner) REFERENCES users(name)
+);
