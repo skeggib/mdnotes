aa74eb7985ab55ec516dfce776333344560aed82
Sebastien Klasa <klasa.sebastien@gmail.com>
2019-02-19 09:05:55 +0100

    Add test project

---
+++ tests/MdNotes.Tests/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MdNotes.Tests
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public User(string name)
+        {
+            Id = -1;
+            Name = name;
+        }
+    }
+}
